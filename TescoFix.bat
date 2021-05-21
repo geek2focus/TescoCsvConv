b09ec02d5506f2c47be8a7ba6f8b6087b29608f7
geek2focus <wilson.andy.d@gmail.com>
2021-05-21 10:27:20 +0100

    Add runner batch file

---
+++ TescoFix.bat
@@ -0,0 +1,15 @@
+@echo off
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set day=%%j
+     set month=%%k
+     set year=%%l
+)
+set output=C:\Users\wilso\Downloads\Tesco-YNAB-%year%-%month%-%day%.csv
+SET /a day=1%day%+1%day%-2%day%
+SET /a month=1%month%+1%month%-2%month%
+SET /a year=%year%-2000
+set input=C:\Users\wilso\Downloads\CreditCard-Transactions-%day%-%month%-%year%.csv
+echo In: %input%
+echo Out: %output%
+TescoCsvConv %input% %output%
