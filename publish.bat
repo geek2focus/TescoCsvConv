6e1ef38a15ada849aa977d37cc9973930a5a5faa
geek2focus <wilson.andy.d@gmail.com>
2021-05-21 10:28:09 +0100

    Copy TescoFix.bat as part of publish

--- publish.bat
+++ publish.bat
@@ -1,2 +1,3 @@
 dotnet publish TescoCsvConv.csproj -c Release --self-contained true -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true
 copy bin\Release\net5.0\win-x64\publish\*.* C:\Util
+copy TescoFix.bat C:\Util
