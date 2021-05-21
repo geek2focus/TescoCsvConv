8b1de70f879548af493a6bd6c77be0b411d36374
Andy Wilson <Andy.Wilson@eu.medical.canon>
2021-05-21 10:56:51 +0100

    Use USERPROFILE instead of hard-coded path

--- TescoFix.bat
+++ TescoFix.bat
@@ -5,11 +5,11 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
      set month=%%k
      set year=%%l
 )
-set output=C:\Users\wilso\Downloads\Tesco-YNAB-%year%-%month%-%day%.csv
+set output=%USERPROFILE%\Downloads\Tesco-YNAB-%year%-%month%-%day%.csv
 SET /a day=1%day%+1%day%-2%day%
 SET /a month=1%month%+1%month%-2%month%
 SET /a year=%year%-2000
-set input=C:\Users\wilso\Downloads\CreditCard-Transactions-%day%-%month%-%year%.csv
+set input=%USERPROFILE%\Downloads\CreditCard-Transactions-%day%-%month%-%year%.csv
 echo In: %input%
 echo Out: %output%
 TescoCsvConv %input% %output%
